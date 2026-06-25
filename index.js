76e61a352efbb88bf98133d2650eb796a3284da7
MrProgress01 <progressaguguo@gmail.com>
2026-06-25 13:00:03 +0100

    fix cors

--- index.js
+++ index.js
@@ -4,16 +4,25 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const userRoute = require("./route/user.Route.js");
 const productRoute = require("./route/Product.Router.js");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+  }),
+);
+
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("hello pace-setter autos your liveserver is on!");
 });
+
 app.use("/api/auth", userRoute);
+app.use(cors());
 app.use("/api/products", productRoute);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
